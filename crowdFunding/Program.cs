2c1a4e1b5f969e2b61dcfc5af869995708e9f343
Mema Stamataki <mema@livemdia.com>
2020-05-15 21:09:25 +0300

    Project Services

--- crowdFunding/Program.cs
+++ crowdFunding/Program.cs
@@ -1,6 +1,9 @@
 ﻿using crowdFunding.Services;
 using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using crowdFunding.Services.Options;
 
 namespace crowdFunding
 {
@@ -10,6 +13,22 @@ namespace crowdFunding
         {
             using (var context = new CrowdFundingDbContext())
             {
+                
+                
+                    IProjectService projectservice = new ProjectService(context);
+                   
+                    var proj = projectservice.CreateProject(new crowdFunding.Services.Options.CreateProjectOption()
+                    { 
+                        Name = "Project 10",
+                        Description = "Testtest",
+                        Category = (Category)3
+                    }
+                    );
+
+                var project = projectservice.SearchProject( new SearchProjectOption
+                {
+                    ProjectId=1,
+                }).FirstOrDefault();
             }
         }
     }
--- crowdFunding/Services/IProjectService.cs
+++ crowdFunding/Services/IProjectService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using crowdFunding.Services.Options;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace crowdFunding.Services
 {
-    interface IProjectService
+   public interface IProjectService
     {
+      Project CreateProject(CreateProjectOption options);
+      IQueryable<Project> SearchProject(SearchProjectOption options);
+      Project UpdateProject(UpdateProjectOption options);
+      Project GetProjectByCategory(Category Category);
+     Project GetProjectByName(string Name);
     }
 }
