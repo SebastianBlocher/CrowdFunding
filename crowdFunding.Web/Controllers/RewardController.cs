6364a3e9738772a9b152b1041831b8b904f740dc
Giorgos Oikonomou <geo.oikonomou.021@gmail.com>
2020-05-20 13:02:38 +0300

    added_api controllers

---
+++ crowdFunding.Web/Controllers/RewardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crowdFunding.Web.Controllers
+{
+    public class RewardController : Controller
+    {
+        [Route("{reward}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
