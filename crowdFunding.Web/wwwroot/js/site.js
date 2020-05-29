0e637c4241e77fff42d26401fd2eef71ee25abd8
odysplat <oplatsakis@athtech.gr>
2020-05-29 18:19:37 +0300

    Search, navbar layout, tending template

--- crowdFunding.Web/wwwroot/js/site.js
+++ crowdFunding.Web/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var btnL = document.getElementById("btnLeft");
+var btnR = document.getElementById("btnRight");
+
+var content = document.getElementById("content");
+
+btnR.addEventListener("click", goRight);
+btnL.addEventListener("click", goLeft);
+
+var clickedIndex = 0;
+
+function goRight() {
+    if (clickedIndex < 2) {
+        clickedIndex = clickedIndex + 1;
+        content.style.marginLeft = -190 * clickedIndex + "px";
+    }
+
+}
+
+function goLeft() {
+    if (clickedIndex > 0) {
+        clickedIndex = clickedIndex - 1;
+        content.style.marginLeft = -190 * clickedIndex + "px";
+
+    }
+}
